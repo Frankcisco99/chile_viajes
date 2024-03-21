237f74fe3e79494d6a8e3d8e8b74f381a9331924
Francisco <ffigueroa@mail.com>
2024-03-21 18:40:23 -0300

    se añade javascript para el menu

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+const isActive = ()=>{
+    window.scrollY > 0 ? document.getElementById("menu").classList.add("bg-color") 
+                        : 
+                        document.getElementById("menu").classList.remove("bg-color")
+}
+
+window.addEventListener("scroll", isActive);
